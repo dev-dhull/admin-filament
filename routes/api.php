6026865879860c09b6f85554200b743ee051ad74
David <103427573+dev-dhull@users.noreply.github.com>
2022-08-09 09:07:33 +0530

    api

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Employee;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Resources\EmployeeResource;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/employees', function(){
+    $employees = Employee::orderBy('last_name')->get();
+
+    return EmployeeResource::collection($employees);
 });
